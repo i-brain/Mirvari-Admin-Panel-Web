07c07595953a003803bd9336594bc707c6d83bd3
Ibrahim Mammadov <manqanat1@gmail.com>
2024-08-03 12:08:44 +0400

    keyboard decimal issue fixed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "3ed1c89c7c8f4bd8fc61fed22f86c1f2",
-"index.html": "b982d07f1194ae759fb04c3d7b9994d5",
-"/": "b982d07f1194ae759fb04c3d7b9994d5",
-"main.dart.js": "37540aa9d240e82920a9eb8b504d7137",
+"index.html": "f1442eeebca903b4fd9066cd1a72904e",
+"/": "f1442eeebca903b4fd9066cd1a72904e",
+"main.dart.js": "73e815b78de644d705bef37131e20696",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
